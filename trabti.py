cc75e3b2cd2bdb792684ea056644dc264092050c
Ricardo Castro <rickvini@unb.br>
2017-10-24 17:41:04 -0200

    novo teste commit

--- trabti.py
+++ trabti.py
@@ -11,13 +11,19 @@ def saida():
 	print('\nComprimento médio:');
 	print('\nTaxa de compressão:');
 
-# Saida
 
 
 #função para compactar
 def comp(arquivo):
-	arq = open(arquivo,'r+')
 	print("modo compactação")
+	qtdBytes = 1
+	with open(arquivo, 'rb') as arq:
+	    byte = arq.read(qtdBytes)
+	    while byte:
+	        byte = arq.read(qtdBytes)
+	        print(byte)
+
+
 
 #função para descompactar
 def descomp(arquivo):
@@ -27,10 +33,14 @@ def descomp(arquivo):
 print("bem vindo ao C-D huffman!")
 modo = input("Em que modo deseja operar?\n(pressione -c para modo compactação e -d para descompactação)")
 
-if(modo=='c'):
+if(modo=='c' or modo=='-c' or modo=='C' or modo=='-C'):
 	arquivo = input("qual arquivo deseja compactar?")
+
+
+
+
 	comp(arquivo)
-elif (modo=='d'):
+elif (modo=='d' or modo=='-d' or modo=='D' or modo=='-D'):
 	arquivo = input("qual arquivo deseja descompactar?")
 	descomp(arquivo)
 else:
